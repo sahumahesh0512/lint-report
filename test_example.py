efe9c799f8bae9808f4c22004f022316fa72f2b7
sahumahesh0512 <91590622+sahumahesh0512@users.noreply.github.com>
2024-06-13 10:27:06 +0530

    Create test_example.py

---
+++ test_example.py
@@ -0,0 +1,21 @@
+# test_example.py
+ 
+import pytest
+
+from example import add, subtract
+ 
+def test_add():
+
+    assert add(1, 2) == 3
+
+    assert add(-1, 1) == 0
+
+    assert add(-1, -1) == -2
+ 
+def test_subtract():
+
+    assert subtract(2, 1) == 1
+
+    assert subtract(1, 1) == 0
+
+    assert subtract(-1, -1) == 0
