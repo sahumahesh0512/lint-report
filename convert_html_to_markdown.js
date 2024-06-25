d75d8a64577bce4d52d1c9478da0fc603303214f
sahumahesh0512 <91590622+sahumahesh0512@users.noreply.github.com>
2024-06-25 17:44:07 +0530

    Update convert_html_to_markdown.js

--- convert_html_to_markdown.js
+++ convert_html_to_markdown.js
@@ -1,21 +1,15 @@
 const fs = require('fs');
-const { htmlToText } = require('html-to-text');
 
 async function run() {
   try {
     const htmlFilePath = './junit.html'; // Path to the HTML file in the root directory
     const htmlContent = fs.readFileSync(htmlFilePath, 'utf8');
 
-    // Convert HTML to markdown
-    const markdownContent = htmlToText(htmlContent, {
-      wordwrap: 130
-    });
-
     const jobSummary = `
       ### Job Summary
-      ${markdownContent}
+      ${htmlContent}
     `;
-    
+
     fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, jobSummary);
   } catch (error) {
     console.error(error);
