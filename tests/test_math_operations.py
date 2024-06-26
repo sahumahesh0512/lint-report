195d68286b15fa9a291eebc7e4c0ad8b0a6fff7d
sahumahesh0512 <91590622+sahumahesh0512@users.noreply.github.com>
2024-06-26 12:46:23 +0530

    Update test_math_operations.py

--- tests/test_math_operations.py
+++ tests/test_math_operations.py
@@ -6,7 +6,7 @@ class TestMathOperations(unittest. TestCase):
     def test_add (self):
         self.assertEqual(add(1,2),3)
         self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(-1,-1), -2)
+        self.assertEqual(add(-1,-1), -3)
  
         
     def test_subtract(self):
